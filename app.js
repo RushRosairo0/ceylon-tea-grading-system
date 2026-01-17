8fd406ae2a7873120c64f708a9fe3b6b27b46cc0
RushRosairo0 <rosairorushmi@gmail.com>
2026-01-17 22:29:38 +0530

    rushmi: express server created

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('hello world...');
+const express = require('express');
+const cors = require('cors');
+const { CORS } = require('./common/messages');
+require('dotenv').config({
+  path: process.env.ENV_PATH || '.env',
+});
+
+const ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+
+// initialize the express app
+const app = express();
+app.use(express.json({ limit: '10kb' }));
+
+// initialize cors
+const allowedOrigins = [process.env.MOBILE_URL || 'http://localhost:5173'];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error(CORS.INVALID));
+      }
+    },
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Access-Token'],
+  }),
+);
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`${ENV} | ${PORT}`);
+});
---
+++ common/messages.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // cors
+  CORS: {
+    INVALID: 'Not allowed by CORS!',
+  },
+};
