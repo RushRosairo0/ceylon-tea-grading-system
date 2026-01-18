7b5fdda874b96c1cc2f04ec75b5c875501c45cae
RushRosairo0 <rosairorushmi@gmail.com>
2026-01-18 19:08:04 +0530

    rushmi: upload image using multer

---
+++ middleware/imageUpload.js
@@ -0,0 +1,43 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  // set destination folder
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+
+  // customize the filname
+  filename: (req, file, cb) => {
+    const date = new Date().toISOString().replace(/:/g, '-');
+    const ext = path.extname(file.originalname);
+    const nameWithoutExt = path.basename(file.originalname, ext);
+
+    cb(null, `${nameWithoutExt}_${date}${ext}`);
+  },
+});
+
+const uploadMiddleware = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+  fileFilter: (req, file, cb) => {
+    file.mimetype.startsWith('image/') ? cb(null, true) : cb(new Error('Only images allowed'));
+  },
+});
+
+const imageUpload = {
+  upload: async (req, res, next) => {
+    const uploadSingle = uploadMiddleware.single('file');
+
+    await new Promise((resolve, reject) => {
+      uploadSingle(req, res, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+
+    next();
+  },
+};
+
+module.exports = imageUpload;
